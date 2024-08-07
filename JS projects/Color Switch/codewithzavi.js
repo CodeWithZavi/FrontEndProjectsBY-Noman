b9d0e0996421118c3c3774705a553ae87aa9f8b0
CodeZavian <nomiemerge9901@gmail.com>
2024-08-07 15:04:38 +0500

    //

---
+++ JS projects/Color Switch/codewithzavi.js
@@ -0,0 +1,15 @@
+const buttons = document.querySelectorAll('.button')
+//console.log(buttons)
+const body = document.querySelector("body")
+
+buttons.forEach(function (button) {  // read every button
+    console.log(button)
+
+
+
+    button.addEventListener('click', function (e) {
+
+
+    });
+});
+
