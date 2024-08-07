4630455da7e05ae78f42a09251521a5dd41297cb
CodeZavian <nomiemerge9901@gmail.com>
2024-08-07 20:47:38 +0500

    //

--- JS projects/BMI index/codewithzavi.js
+++ JS projects/BMI index/codewithzavi.js
@@ -1,3 +1,19 @@
-const
+// we cannot input value before submit cuz it will give empty value
+const form = document.querySelector('form');
 
-// stop form from 1st default action
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    const height = parseInt(document.querySelector('#height').value);
+    const weight = parseInt(document.querySelector('#weight').value);
+    const resultInput = document.querySelector('#bmi');
+
+    if (!height || height <= 0 || isNaN(height)) {
+        resultInput.value = "Enter valid height";
+    } else if (!weight || weight <= 0 || isNaN(weight)) {
+        resultInput.value = "Enter valid weight";
+    } else {
+        const bmi = (weight / ((height * height) / 10000)).toFixed(2);
+        resultInput.value = bmi;
+    }
+});
